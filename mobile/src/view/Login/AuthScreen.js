2dc0ca4e98ec343ecb866b6d62596dec95e78eb4
Douglas Nicolas <dnmsmpvp@gmail.com>
2021-10-01 09:10:56 -0300

    Adding suport to tiny phones

--- mobile/src/view/Login/AuthScreen.js
+++ mobile/src/view/Login/AuthScreen.js
@@ -7,6 +7,7 @@ import {
   SafeAreaView,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 
 import Logo from "../../assets/Head.png";
@@ -14,27 +15,40 @@ import Ilustration from "../../assets/Login-Image.png";
 
 export default AuthScreen = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Image source={Logo} style={styles.logo} />
-      <Image source={Ilustration} style={{ marginTop: 20 }} />
+    <SafeAreaView centerContent={true} style={styles.container}>
+      <ScrollView
+        style={{ width: "100%" }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Image source={Logo} style={styles.logo} />
+        <Image source={Ilustration} style={styles.ilustration} />
 
-      <TouchableOpacity style={styles.button1}>
-        <Text style={styles.button1Text}>Sign in with facebook</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button2}>
-        <Text style={styles.button2Text}>Sign in with Google</Text>
-      </TouchableOpacity>
-
-      <View style={{ width: "100%", marginTop: 20 }}>
-        <Text style={styles.label}>Email</Text>
-        <TextInput style={styles.input} placeholder="Type your email here..." />
-        <Text style={styles.label}>Password</Text>
-        <TextInput style={styles.input} placeholder="Type your password here..." />
-
-        <TouchableOpacity style={styles.button3}>
-          <Text style={styles.button3Text}>Sign In</Text>
+        <TouchableOpacity style={styles.button1}>
+          <Text style={styles.button1Text}>Sign in with Facebook</Text>
         </TouchableOpacity>
-      </View>
+        <TouchableOpacity style={styles.button2}>
+          <Text style={styles.button2Text}>Sign in with Google</Text>
+        </TouchableOpacity>
+
+        <View style={{ borderColor: "#ddd", borderWidth: 1 }} />
+
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Type your email here..."
+          />
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Type your password here..."
+          />
+
+          <TouchableOpacity style={styles.button3}>
+            <Text style={styles.button3Text}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -45,6 +59,15 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     marginRight: 30,
     marginTop: 50,
+  },
+
+  logo: {
+    alignSelf: "center",
+  },
+
+  ilustration: {
+    alignSelf: "center",
+    marginTop: 20,
   },
 
   button1: {
@@ -70,12 +93,18 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 50,
     marginTop: 20,
+    marginBottom: 20,
     borderRadius: 5,
   },
 
   button2Text: {
     color: "#333",
     fontFamily: "Poppins_500Medium",
+  },
+
+  inputGroup: {
+    width: "100%",
+    marginTop: 20,
   },
 
   input: {
@@ -86,11 +115,13 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderColor: "#666",
     color: "#333",
+    marginBottom: 20,
   },
 
   label: {
     color: "#333",
-    fontFamily: "Poppins_500Medium"
+    fontFamily: "Poppins_500Medium",
+    marginBottom: 2,
   },
 
   button3: {
@@ -100,11 +131,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#333",
-    marginTop: 20
+    marginTop: 10,
   },
 
   button3Text: {
     color: "#fff",
-    fontFamily: "Poppins_500Medium"
-  }
+    fontFamily: "Poppins_500Medium",
+  },
 });
