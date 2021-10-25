cd95bba08c9a06f27d3226662f0f405f255bb203
Douglas Nicolas <dnmsmpvp@gmail.com>
2021-10-25 18:34:23 -0300

    Unhatching the backend

--- backend/src/repositories/BookRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Book } from "../database/entities/Book";
-
-@EntityRepository(Book)
-class BookRepository extends Repository<Book> {}
-
-export { BookRepository };
